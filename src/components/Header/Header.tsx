97cb1f63784da71d6f1c290496948d5876af95c2
Keian Kaserman <278810033+keiank@users.noreply.github.com>
2026-09-19 19:09:09 -0400

    Add hamburger menu and basic mobile header layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import "./Header.css";
 import profile from "../../data/profile";
+import { useState } from "react";
 
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
       <a className="header__brand" href="#home">
@@ -21,6 +24,24 @@ export function Header() {
           Contact
         </a>
       </nav>
+      <button
+        aria-expanded={isOpen}
+        aria-controls="header__nav"
+        aria-label="Toggle navigation menu"
+        onClick={() => setTimeout(() => setIsOpen(!isOpen), 100)}
+        className="header__hamburger-button"
+        type="button"
+      >
+        {!isOpen
+        ? <svg className="header__mobile-menu" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4.00073 4.99915H19.9991M4.00073 11.9999H19.9991M4.00073 19.0007H19.9991" stroke="var(--color-text-primary)" stroke-width="2" stroke-linecap="round"/>
+          </svg>
+        : <svg className="header__mobile-menu" width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.751 7.75056L7.75056 13.751M7.75056 7.75056L13.751 13.751M20.7516 10.7508C20.7516 16.2741 16.2741 20.7516 10.7508 20.7516C5.22751 20.7516 0.75 16.2741 0.75 10.7508C0.75 5.22751 5.22751 0.75 10.7508 0.75C16.2741 0.75 20.7516 5.22751 20.7516 10.7508Z" stroke="#808080" stroke-width="1.5" stroke-linecap="round"/>
+          </svg>
+        }
+      </button>
+      
     </header>
   );
 }
